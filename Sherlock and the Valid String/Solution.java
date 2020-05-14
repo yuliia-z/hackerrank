d672db6b2bf42bd5bf5c7f0a2a25f3fe9e4b7994
yuliia-zabudska <zabudska.juliia@gmail.com>
2020-05-14 11:35:55 +0300

    add 'sherlock and the valid string' solution

--- Sherlock and the Valid String/Solution.java
+++ Sherlock and the Valid String/Solution.java
@@ -1,18 +1,37 @@
 import java.io.*;
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 public class Solution {
 
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String result = isValid(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
     // Complete the isValid function below.
     static String isValid(String s) {
-        List<Character> chars = s.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        if (s.length() == 1) return "YES";
 
-        Map<Character, Long> groupedChars = chars.stream().collect(groupingBy(Function.identity(), counting()));
+        Map<Character, Long> groupedChars = s.chars()
+                .mapToObj(e -> (char) e)
+                .collect(groupingBy(Function.identity(), counting()));
 
         Long min = Collections.min(groupedChars.values());
         Long max = Collections.max(groupedChars.values());
@@ -20,31 +39,14 @@ public class Solution {
         long countMin = groupedChars.values().stream().filter(val -> val.equals(min)).count();
         long countMax = groupedChars.values().stream().filter(val -> val.equals(max)).count();
 
-        // FIXME
         if (countMax == groupedChars.size() || countMin == groupedChars.size()) {
             return "YES";
-        } else if (countMax == groupedChars.size()-1 || countMin == groupedChars.size()-1) {
-            return "YES";
+        } else if (countMax == groupedChars.size() - 1) {
+            return min == 1 || max - min == 1 ? "YES" : "NO";
+        } else if (countMin == groupedChars.size() - 1) {
+            return max - min == 1 ? "YES" : "NO";
         } else {
             return "NO";
         }
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-//
-//        String s = scanner.nextLine();
-
-        String result = isValid("aaaabbcc");
-        System.out.println(result);
-
-//        bufferedWriter.write(result);
-//        bufferedWriter.newLine();
-//
-//        bufferedWriter.close();
-//
-//        scanner.close();
     }
 }
