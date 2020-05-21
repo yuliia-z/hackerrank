58b2b710558614ca23f3aac360cc1c585c88e343
yuliia-zabudska <zabudska.juliia@gmail.com>
2020-05-21 12:15:17 +0300

    update 'array manipulation' solution

--- Array Manipulation/Solution.java
+++ Array Manipulation/Solution.java
@@ -3,61 +3,56 @@ import java.util.*;
 
 public class Solution {
 
-    // Complete the arrayManipulation function below.
-    static long arrayManipulation(int n, int[][] queries) {
-        int[] result = new int[n];
-
-        for (int[] query : queries) {
-            int a = query[0]-1;
-            int b = query[1];
-            int k = query[2];
-
-            for (int j = a; j < b; j++) {
-                result[j] = result[j] + k;
-            }
-        }
-
-        Arrays.sort(result);
-
-        return result[result.length-1];
-    }
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-//
-//        String[] nm = scanner.nextLine().split(" ");
-//
-//        int n = Integer.parseInt(nm[0]);
-//
-//        int m = Integer.parseInt(nm[1]);
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
 
         int[][] queries = new int[3][3];
-//
-//        for (int i = 0; i < m; i++) {
-//            String[] queriesRowItems = scanner.nextLine().split(" ");
-//            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-//
-//            for (int j = 0; j < 3; j++) {
-//                int queriesItem = Integer.parseInt(queriesRowItems[j]);
-//                queries[i][j] = queriesItem;
-//            }
-//        }
 
-        queries = new int[][]{
-                {1,2, 100},
-                {2, 5, 100},
-                {3, 4, 100}
-        };
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        long result = arrayManipulation(4000, queries);
-//
-//        bufferedWriter.write(String.valueOf(result));
-//        bufferedWriter.newLine();
-//
-//        bufferedWriter.close();
-//
-//        scanner.close();
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+        long[] arr = new long[n];
+        long maxSum = 0;
+        for (int[] query : queries) {
+            int from = query[0];
+            int to = query[1];
+            long val = query[2];
+            arr[from - 1] += val;
+            if (to < n) arr[to] -= val;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] += arr[i - 1];
+            if (arr[i] > maxSum) maxSum = arr[i];
+        }
+
+        return maxSum;
     }
 }
